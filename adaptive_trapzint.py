f47c13df4e7c1a49174f3fb321604051157a020b
patti102 <patti102@mail.chapman.edu>
2016-02-05 16:24:28 -0800

    Update adaptive_trapzint.py

--- adaptive_trapzint.py
+++ adaptive_trapzint.py
@@ -69,7 +69,7 @@ def curve(x):
 def test_adaptive_limits():
     """ensures that the n value given is sufficient for the curve function.
     This function has calculated n of 366 on the interval 0 to 2"""
-    t = symbols('t')
+    t = var('t')
     curve_func = t**2 + t + 1
     integ = integrate(curve_func, t)
     value = lambdify([t], integ)
